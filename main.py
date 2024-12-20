e37331f73710bef98589690e56bfda896edfbdff
Aditya S <aditya.srikanth1511@gmail.com>
2024-12-20 20:25:03 +0530

    model evaluation component added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.textSummarizer.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.textSummarizer.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 
@@ -31,6 +32,17 @@ try:
     logger.info(f"Stage {STAGE_NAME} initiated")
     model_trainer_pipeline=ModelTrainerTrainingPipeline()
     model_trainer_pipeline.initiate_model_trainer()
+    logger.info(f"Stage {STAGE_NAME} completed")
+
+except Exception as e:
+    raise e
+
+STAGE_NAME="Model Evaluation Stage"
+
+try:
+    logger.info(f"Stage {STAGE_NAME} initiated")
+    model_evaluation_pipeline=ModelEvaluationTrainingPipeline()
+    model_evaluation_pipeline.initiate_model_evaluation()
     logger.info(f"Stage {STAGE_NAME} completed")
 
 except Exception as e:
